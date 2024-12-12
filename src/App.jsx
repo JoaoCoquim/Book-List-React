0f45ae01f79a18b0a09ab131b97af764962c91db
JoaoCoquim <joaocoquim@gmail.com>
2024-12-12 17:24:52 +0000

    Fix base path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [livros, setLivros] = useState(listaLivrosJSON);
 
   return (<>
-      <Routes>
+      <Routes basename="/Book-List-React">
         <Route path="/" element={<Home livros={livros} setLivros={setLivros} />} />
         <Route path="/create" element={<Create livros={livros} setLivros={setLivros} />} />
         <Route path="/view/:id" element={<View livros={livros} />} />
